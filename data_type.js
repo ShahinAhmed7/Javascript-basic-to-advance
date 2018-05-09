8a9b16b1fe74d042229df25cc9e53846d5ca1583
Shahin Ahmed <meshahin007@gmail.com>
2018-05-09 21:25:15 +0600

    commit

---
+++ data_type.js
@@ -0,0 +1,26 @@
+
+
+// multi variable 
+/*
+var a = 12, b = 13, c = 14, d = 15;
+console.log(a,b,c,d);
+*/
+
+
+// shortcode variable
+/*
+var a = b = c = 2;
+var d = 10;
+console.log(a, d);
+*/
+
+/*
+var earthIsRound = false;
+console.log(earthIsRound);
+*/
+
+var pie = 4.5857;
+var hello = "Hello World";
+var earthIsRound = false;
+
+console.log(typeof earthIsRound)
