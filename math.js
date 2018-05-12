6990ec20b3d0cb3c35515fc9cf260c543b506103
Shahin Ahmed <meshahin007@gmail.com>
2018-05-12 15:29:43 +0600

    commit

---
+++ math.js
@@ -0,0 +1,14 @@
+
+
+/*
+var eggPrice = 5;
+var numberOfEgg = 10;
+var totalofPrice = eggPrice * numberOfEgg;
+
+console.log('Total Egg Price', totalofPrice)
+*/
+
+var usdToBdt = 83.5;
+var bdt = 5000;
+var usd = bdt / usdToBdt;
+console.log(usd);
