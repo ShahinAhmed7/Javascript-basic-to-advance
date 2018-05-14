bf9c2676f16a83ce1e7ae4503fe329e416d7b0a8
Shahin Ahmed <meshahin007@gmail.com>
2018-05-14 09:27:57 +0600

    commit

--- comparison.js
+++ comparison.js
@@ -7,11 +7,28 @@
 // <=
 // >=
 
-var n = 10;
-var m = 12;
+// var n = 10;
 
-if(n>m) {
-    console.log('N is smaler than 12');
+// if (n>12) {
+//     console.log(n, 'Is smaller than 12');
+// } else if (n<12){
+//     console.log(n, 'greater than 12')
+// }
+
+// var n = 21;
+// var m = 22;
+
+// if (n>m){
+//     console.log(n, 'Greater than', m);
+// } else{
+//     console.log(n, 'Smaller than', m);
+// }
+
+
+var n = 10;
+
+if(n==10) {
+    console.log(n, 'equal 10');
 } else{
-    console.log(' is greater than 12');
+    console.log(n, 'not equal 10');
 }
