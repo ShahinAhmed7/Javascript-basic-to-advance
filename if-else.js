1a789b05d0ed9795a6b87f9465bee4ebaae3fe94
Shahin Ahmed <meshahin007@gmail.com>
2018-05-16 20:17:14 +0600

    commit

--- if-else.js
+++ if-else.js
@@ -1,11 +1,24 @@
 
 
 
-var n = -12;
+// var n = -12;
 
-if (n>0) {
-    var result = "This number " +n+ " is Positive ";
+// if (n>0) {
+//     var result = "This number " +n+ " is Positive ";
+// } else{
+//     var result = "This number " + n + " is Negative ";
+// }
+// console.log(result);
+
+var n = 11;
+var result;
+
+var remiender = n % 2;
+
+if(0 == remiender) {
+    result = "This number is even";
 } else{
-    var result = "This number " + n + " is Negative ";
+    result = "This number is not odd";
 }
+
 console.log(result);
---
+++ ternary.js
@@ -0,0 +1,10 @@
+
+
+var n = 11;
+var result;
+
+var remainder = n % 2;
+
+result = (0 == remainder) ? "Even" : "Odd";
+
+console.log("This number is", result);
