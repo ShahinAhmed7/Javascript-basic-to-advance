b6d05ff449fc1c3c60b7fb14744ecb40a96adab1
Shahin Ahmed <meshahin007@gmail.com>
2018-05-18 17:58:27 +0600

    commit

---
+++ while-loop.js
@@ -0,0 +1,43 @@
+
+
+
+// var i = 10;
+
+// while(i -- > 0) {
+//     console.log(i);
+// }
+
+
+
+
+// var i = 10;
+
+// while(i -- > 0) {
+//     console.log(i);
+// }
+
+
+
+
+
+// var i = 0;
+
+// while(i < 10) {
+//     i++;
+//     console.log(i);
+// }
+
+
+
+
+
+// var i = 0;
+
+// while(true) {
+//     console.log(i);
+
+//     if(10 == i) {
+//         break;
+//     }
+//     i++;
+// }
