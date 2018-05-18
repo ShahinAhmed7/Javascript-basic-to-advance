cf7d3068ce99b132ad499358bee3b995edb9fbaa
Shahin Ahmed <meshahin007@gmail.com>
2018-05-19 00:05:24 +0600

    commit

---
+++ forloop.js
@@ -0,0 +1,25 @@
+
+
+// var i;
+
+// for(i = 0; i < 10 ; i++) {
+//     console.log(i);
+// }
+
+var i;
+
+for (i = 0; i < 10; i += 1) {
+    console.log(i);
+}
+
+
+
+
+// var i;
+// for (i = 0; i < 50; i += 1) {
+//     console.log(i);
+
+//     if (10 == i) {
+//         break;
+//     }
+// }
